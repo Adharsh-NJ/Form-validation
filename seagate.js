53cb965943fd6e47453b93c501243cbea27dc120
Adharsh nj <adharshnj@gmail.com>
2022-04-08 14:44:43 +0530

    fixed bugs in form validation

--- seagate.js
+++ seagate.js
@@ -1,29 +1,91 @@
 let search = document.querySelector("#search");
 let searchContainer = document.querySelector("#search-container");
-let inquiryPurposeSelect = document.querySelector("#inquiry-purpose");
-let firstNameInput = document.querySelector("#first-name-input");
-let lastNameInput = document.querySelector("#last-name-input");
-let companyEmailInput = document.querySelector("#company-email-input");
-let companyNameInput = document.querySelector("#company-name-input");
-let roleSelect = document.querySelector("#role");
-let phoneNumberInput = document.querySelector("#phone-number-input");
-let purchasePlanSelect = document.querySelector("#purchase-plan-select");
-let countrySelect = document.querySelector("#country-select");
-let policyCheckbox = document.querySelector("#company-policy");
-let newsLetterCheckbox = document.querySelector("#newsLetter");
 let additionalDetails = document.querySelector("#additional-details");
 let form = document.querySelector("#details-form");
-let detailsForm = document.querySelector("#details-form");
 let submitButton = document.querySelector("#submit-button");
-let isValidPurpose = false;
-let isValidFirstName = false;
-let isValidLastName = false;
-let isValidCompanyName = false;
-let isValidCompanyEmail = false;
-let isValidRole = false;
-let isValidPhone = false;
-let isValidPurchase = false;
-let isValidCountry = false;
+let validation = {
+    purpose: {
+        element: document.querySelector("#inquiry-purpose"),
+        pattern() {
+            return !!this.element.value
+        },
+        isValid: false
+    },
+    fname: {
+        element: document.querySelector("#first-name-input"),
+        pattern() {
+            return this.element?.value.length >= 4
+        },
+        message: "first name should be atleast four letters",
+        isValid: false
+    },
+    lname: {
+        element: document.querySelector("#last-name-input"),
+        pattern() {
+            return this.element.value.length >= 4
+        },
+        message: "last name should be atleast four letters",
+        isValid: false
+    },
+    companyEmail: {
+        element: document.querySelector("#company-email-input"),
+        pattern() {
+            return this.element.value.includes("@")
+        },
+        isValid: false
+    },
+    companyName: {
+        element: document.querySelector("#company-name-input"),
+        pattern() {
+            return this.element.value.length >= 4
+        },
+        message: "company name should be atleast four letters",
+        isValid: false
+    },
+    role: {
+        element: document.querySelector("#role"),
+        pattern() {
+            return !!this.element.value
+        },
+        isValid: false
+    },
+    phoneNumber: {
+        element: document.querySelector("#phone-number-input"),
+        pattern() {
+            return this.element.value.length === 10
+        },
+        message: "Phone number should be 10 digit",
+        isValid: false
+    },
+    purchaseTimeline: {
+        element: document.querySelector("#purchase-plan-select"),
+        pattern() {
+            return !!this.element.value
+        },
+        isValid: false
+    },
+    country: {
+        element: document.querySelector("#country-select"),
+        pattern() {
+            return !!this.element.value
+        },
+        isValid: false
+    },
+    newsLetter: {
+        element: document.querySelector("#newsLetter"),
+        pattern() {
+            return this.element.checked
+        },
+        isValid: false
+    },
+    policy: {
+        element: document.querySelector("#company-policy"),
+        pattern() {
+            return this.element.checked
+        },
+        isValid: false
+    }
+}
 
 //search button functionality
 search.addEventListener("click", () => {
@@ -35,97 +97,60 @@ countries.map((value) => {
     let option = document.createElement("option");
     let optionName = document.createTextNode(value.name);
     option.appendChild(optionName);
-    countrySelect.appendChild(option);
+    validation.country.element.appendChild(option);
 })
 
 //validation
-inquiryPurposeSelect.addEventListener("input", () => {
-    isValidPurpose = inquiryPurposeSelect.value ? true : false;
-    onError(inquiryPurposeSelect, isValidPurpose);
+validation.phoneNumber.element.addEventListener("keypress", (e) => {
+    if (["e", "E", "+", "-"].includes(e.key)) {
+        e.preventDefault()
+    }
 })
 
-firstNameInput.addEventListener("input", () => {
-    isValidFirstName = firstNameInput.value.length >= 4;
-    onError(firstNameInput, isValidFirstName);
-    showError();
-})
-
-lastNameInput.addEventListener("input", () => {
-    isValidLastName = lastNameInput.value.length >= 4;
-    onError(lastNameInput, isValidLastName);
-    showError();
-})
-
-companyEmailInput.addEventListener("input", () => {
-    isValidCompanyEmail = companyEmailInput.value.includes("@");
-    onError(companyEmailInput, isValidCompanyEmail);
-    showError();
-})
-
-companyNameInput.addEventListener("input", () => {
-    isValidCompanyName = companyNameInput.value.length >= 4;
-    onError(companyNameInput, isValidCompanyName);
-    showError();
-})
-
-roleSelect.addEventListener("input", () => {
-    isValidRole = roleSelect.value;
-    onError(roleSelect, isValidRole);
-})
-
-phoneNumberInput.addEventListener("input", () => {
-    isValidPhone = phoneNumberInput.value.length === 10 && !phoneNumberInput.value.includes("e") ;
-    onError(phoneNumberInput, isValidPhone);
-    showError();
-})
-
-purchasePlanSelect.addEventListener("input", () => {
-    isValidPurchase = purchasePlanSelect.value ? true : false;
-    onError(purchasePlanSelect, isValidPurchase);
-})
-
-countrySelect.addEventListener("input", () => {
-    isValidCountry = countrySelect.value ? true : false;
-    onError(countrySelect, isValidCountry);
-})
-
-form.addEventListener("submit", (e) => {
-    isValidCompanyEmail && isValidCompanyName && isValidCountry
-        && isValidFirstName && isValidLastName && isValidPhone
-        && isValidPurchase && isValidPurpose && isValidRole
-        && newsLetterCheckbox.checked && policyCheckbox.checked ?
-        onSubmit() : showError();
-    e.preventDefault();
-})
-
-
-
-//Throwing errors
-function showError() {
-    !isValidFirstName ? firstNameInput.setCustomValidity("first name should be atleast four letters") : firstNameInput.setCustomValidity("");
-    !isValidLastName ? lastNameInput.setCustomValidity("last name should be atleast four letters") : lastNameInput.setCustomValidity("");
-    !isValidPhone ? phoneNumberInput.setCustomValidity("phone number should be 10 digits") : phoneNumberInput.setCustomValidity("");
-    !isValidCompanyName ? companyNameInput.setCustomValidity("company name should be atleast 4 letters") : companyNameInput.setCustomValidity("");
+for (let key in validation) {
+    validation[key].element.addEventListener("input", () => {
+        validation[key].isValid = validation[key].pattern();
+        showError(validation[key].element, validation[key].isValid, validation[key]?.message)
+    })
 }
 
-function onError(elem, valid) {
-    valid ? elem.classList.remove("error") : elem.classList.add("error");
+//handle submit
+form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    Object.values(validation).filter(value => !value.isValid).length === 0 ?
+        onSubmit() : showError("submit");
+})
+
+submitButton.addEventListener("click", () => {
+    //to show all invalid fields red on submit 
+    Object.values(validation).map((value) => {
+        if (!value.isValid) {
+            value.element.classList.add("error");
+        }
+    })
+})
+
+//Throwing errors
+function showError(elem, status, message) {
+    if (message) {
+        !status ? elem.setCustomValidity(message) : elem.setCustomValidity("");
+        !elem.value && elem.setCustomValidity("Please fill in this filed");
+        status ? elem?.classList.remove("error") : elem?.classList.add("error");
+        elem?.reportValidity();
+    } else {
+        status ? elem.classList?.remove("error") : elem.classList?.add("error");
+    }
 }
 
 //handling submit
 function onSubmit() {
     let data = {
-        purpose: inquiryPurposeSelect.value,
-        firstName: firstNameInput.value,
-        lastName: lastNameInput.value,
-        companyEmail: companyEmailInput.value,
-        companyName: companyNameInput.value,
-        roleInCompany: roleSelect.value,
-        phoneNumber: phoneNumberInput.value,
-        purchaseTimeline: purchasePlanSelect.value,
-        country: countrySelect.value,
         additionalDetails: additionalDetails.value
     }
+    Object.entries(validation).map(value => {
+        let key = value[0];
+        data[key] = value[1].element.value;
+    })
     console.log(data);
 }
 
